1862cc2a7e8d2fa29a45035f6fdf21a6e98552d0
DNiraj13 <63972967+DNiraj13@users.noreply.github.com>
2021-09-06 17:55:16 +0530

    Update TestRunner.java

--- ClarkUIProject/src/test/java/runner/TestRunner.java
+++ ClarkUIProject/src/test/java/runner/TestRunner.java
@@ -3,6 +3,8 @@ package runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import java.io.IOException;
+import java.util.stream.Stream;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -14,4 +16,21 @@ import org.junit.runner.RunWith;
         plugin = { "pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class TestRunner {
+        
+         private static String[] defaultOptions={
+            "--glue", "stepdefs",
+            "--tags", "not @wip", 
+            "--tags", "not @ignore",
+            "--tags", "not @merged",
+            "--tags", "not @STB_SQL_DB_CONN",
+            "--plugin", "pretty",
+            "--plugin", "junit:target/cucumber-report/Cucumber.xml",
+    };
+    
+    public static void main(String[] args) throws IOException{
+        Stream<String> cucumberOptions = Stream.concat(Stream.of(defaultOptions),Stream.of(args));
+        io.cucumber.core.cli.Main.main(cucumberOptions.toArray(String[]::new));
+    }
+        
+        
 }
